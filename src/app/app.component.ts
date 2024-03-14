3057af57ec5051a57a012656e02855c7b8b7d4a0
James Patching <james.patching@asuresoftware.com>
2024-03-13 21:42:11 -0500

    feat: implemented switch user highlighting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UserTypeService } from './user-type.service';
 
 @Component({
@@ -6,11 +6,18 @@ import { UserTypeService } from './user-type.service';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css', 
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'dynamic-nav-app';
+  currentUserType!: string;
 
   constructor(private userTypeService: UserTypeService) {}
 
+  ngOnInit() {
+    this.userTypeService.currentUserType$.subscribe((userType) => {
+      this.currentUserType = userType;
+    });
+  }
+  
   switchUserType(userType: string) {
     this.userTypeService.setUserType(userType);
   }
